725b4cc05659e3703f7df21944e5b0d794bfae3f
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-01-05 16:35:48 +0530

    commit

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Manav Deep Singh Lamba on 05/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

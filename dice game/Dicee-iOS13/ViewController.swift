0869e113b5e2a6d537bc8cd22e4dff5e6494ebb4
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-01-06 08:44:37 +0530

    commit

--- dice game/Dicee-iOS13/ViewController.swift
+++ dice game/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        let randomInt1 = Int.random(in: 1..<7)
+        let randomInt2 = Int.random(in: 1..<7)
+        switch(randomInt1)
+        {
+        case 1 :diceImageView1.image = UIImage(named:"DiceOne")
+            break
+        case 2 :diceImageView1.image = UIImage(named:"DiceTwo")
+            break
+        case 3 :diceImageView1.image = UIImage(named:"DiceThree")
+            break
+        case 4 :diceImageView1.image = UIImage(named:"DiceFour")
+            break
+        case 5 :diceImageView1.image = UIImage(named:"DiceFive")
+            break
+        case 6 :diceImageView1.image = UIImage(named:"DiceSix")
+            break
+        default: diceImageView1.image = UIImage(named:"DiceTwo")
+            break
+        }
 
+        
+        switch(randomInt2)
+        {
+        case 1 :diceImageView2.image = UIImage(named:"DiceOne")
+            break
+        case 2 :diceImageView2.image = UIImage(named:"DiceTwo")
+            break
+        case 3 :diceImageView2.image = UIImage(named:"DiceThree")
+            break
+        case 4 :diceImageView2.image = UIImage(named:"DiceFour")
+            break
+        case 5 :diceImageView2.image = UIImage(named:"DiceFive")
+            break
+        case 6 :diceImageView2.image = UIImage(named:"DiceSix")
+            break
+        default: diceImageView2.image = UIImage(named:"DiceTwo")
+            break
+        }
+    }
+    
 }
 

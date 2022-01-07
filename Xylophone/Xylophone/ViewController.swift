79c240cd93f37d2e3acaef7054bdd8737fb5acc3
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-01-08 02:06:53 +0530

    commit

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        var soundWord = sender.currentTitle
+        playSound(sound: soundWord!)
+    }
+    
+    func playSound(sound : String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}

92ba31014321d17011ddfd3e2beed668cf6c9a50
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-01-11 21:53:55 +0530

    commit

---
+++ Quizzler/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var index = 0
+    let quiz = [
+        ["Four + Two = Six","True"],
+        ["Three + Seven = Six","False"],
+        ["Eleven + Thirteen = Twenty Four","True"]
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        printQues()
+    }
+
+    func printQues(){
+        questionLabel.text = quiz[index][0]
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if (sender.currentTitle == quiz[index][1])
+        {
+            print("Correct")
+        }
+        else
+        {
+            print("Incorrect")
+        }
+        if index<quiz.count-1{
+            index += 1
+        }
+        printQues()
+    }
+}
